a58fd5939795201c60e8e583a852517d2ac287ed
lwatany <loay.watany@gmail.com>
2020-09-23 05:32:06 +0200

    Added validation to inputs

--- src/app/registration/registration.component.ts
+++ src/app/registration/registration.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-registration',
@@ -6,6 +7,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./registration.component.scss']
 })
 export class RegistrationComponent implements OnInit {
+
+  registerForm = new FormGroup({
+    'first_name': new FormControl(null, [Validators.required, Validators.minLength(3), Validators.maxLength(8)]),
+    'last_name': new FormControl(null, [Validators.required, Validators.minLength(3), Validators.maxLength(8)]),
+    'email': new FormControl(null, [Validators.email, Validators.required]),
+    'password': new FormControl(null, [Validators.pattern(/^[A-Z]/), Validators.required]) 
+  });
+
+  getRegisterInfo(registerForm){
+    console.log(registerForm);
+  }
 
   constructor() { }
 
